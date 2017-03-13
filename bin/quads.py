dc773588b7d4f991ea6d51c312fd7d7b406a674c
kyle <kyle@localhost.localdomain>
2017-03-13 11:17:30 -0400

    Further work on extenstion for hil
    define cloud now creates project and network in hil

--- bin/quads.py
+++ bin/quads.py
@@ -7,6 +7,7 @@ import yaml
 import argparse
 import os
 import sys
+import json
 import logging
 import requests
 from subprocess import call
@@ -221,8 +222,9 @@ def main(argv):
         exit(0)
 
     if args.cloudresource:
-        quads.quads_rest_call('PUT', hil_url, '/project/' + args.cloudresource)
-        quads.quads_rest_call('GET', hil_url, '/projects')
+        quads.quads_rest_call('PUT', hil_url, '/project/' + args.cloudresource)     #EC528 addition
+        quads.quads_rest_call('GET', hil_url, '/projects')      #EC528 addition
+        quads.quads_rest_call('PUT', hil_url, '/network/' + args.cloudresource, json.dumps({"owner": args.cloudresource, "access": args.cloudresource, "net_id": ""}))
         quads.quads_update_cloud(args.cloudresource, args.description, args.force, args.cloudowner, args.ccusers, args.cloudticket, args.qinq)
         exit(0)
 
